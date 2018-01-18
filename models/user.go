1d65c23b4014d611acb4b48a15e1095662bc64ca
Sakib Sami <s4kibs4mi@gmail.com>
2018-01-18 14:19:40 +0600

    Models implemented

--- models/user.go
+++ models/user.go
@@ -1,9 +1,30 @@
 package models
 
+import "time"
+
+const (
+	Parent UserType = "parent"
+	Family UserType = "family"
+	Ghost  UserType = "ghost"
+)
+
+const (
+	Registered UserStatus = "registered"
+	Verified   UserStatus = "verified"
+	Blocked    UserStatus = "blocked"
+)
+
+type UserType string
+type UserStatus string
+
 type User struct {
-	ID       string
-	Name     string
-	Username string
-	Password string
-	details  string
+	ID         string
+	Name       string
+	Username   string
+	Password   string
+	details    string
+	UserType   UserType
+	UserStatus UserStatus
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
 }
