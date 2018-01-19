7c0fb1b55e0fc17413db82f0e3bcefe995110166
Sakib Sami <s4kibs4mi@gmail.com>
2018-01-20 00:49:08 +0600

    Proto files for comment defined & service

--- models/comment.go
+++ models/comment.go
@@ -3,13 +3,12 @@ package models
 import "time"
 
 type Comment struct {
-	ID              string
-	UserID          string
-	PostID          string
-	ParentCommentID string
-	Title           string
-	Body            string
-	CreatedAt       time.Time
-	UpdatedAt       time.Time
-	Favourites      int64
+	ID         string
+	UserID     string
+	PostID     string
+	Title      string
+	Body       string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	Favourites int64
 }
